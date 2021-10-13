58a48490937674fdb2d829ca4379ddad68437b92
Igor Mendes <igor.selagem@gmail.com>
2021-10-13 00:21:46 -0300

    Atividade sobre framework, 0 mods

---
+++ src/pesquisa/Pesquisa.java
@@ -0,0 +1,17 @@
+package pesquisa;
+Framework é um termo inglês que, em sua tradução direta, significa estrutura. De maneira geral, essa estrutura é feita para resolver um problema específico.Na programação, um framework é um conjunto de códigos genéricos capaz de unir trechos de um projeto de desenvolvimento. 
+
+Vantagens:
+-Capacidade de adaptação a diversos contextos. 
+-Qualquer um que tenha conhecimento das informações contidas nos códigos podem edita-los.
+-Programa limpo e com respostas mias rápidas.
+
+Desvantagens:
+-Dificuldade de configuração do mesmo.
+-Utilizar vários frameworks num mesmo código, você corre o risco de perder todo o trabalho.
+-Códigos desnecessáros podem fazer com que prejudiquem o desenvolvimento do seu projeto final.
+
+3 exemplos de frameworks Java:
+-Spring
+-Play
+-Struts
